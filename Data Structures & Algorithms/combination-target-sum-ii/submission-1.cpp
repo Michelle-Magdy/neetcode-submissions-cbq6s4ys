ce98cf7a34022181ad56fd90764a8a100125b378
Michelle Magdy <80703891+Michelle-Magdy@users.noreply.github.com>
2026-04-01 02:45:38 +0200

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    vector<vector<int>> res;
+    void dfs(vector<int>& nums,int target,int tempVal,int i,vector<int>& comb){
+        if(target == tempVal){
+            res.push_back(comb);
+            return;
+        }
+        if(i >= nums.size() || target < tempVal){
+            return;
+        }
+        comb.push_back(nums[i]);
+        dfs(nums,target,tempVal + nums[i],i+1,comb);
+        comb.pop_back();
+        while(i+ 1 < nums.size() && nums[i]==nums[i+1])i++;
+        dfs(nums,target,tempVal,i+1,comb);
+        return;
+    }
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<int> temp;
+        dfs(candidates,target,0,0,temp);
+        return res;
+        
+    }
+};
